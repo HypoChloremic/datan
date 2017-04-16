b9c62b0da402f4ae1c9cd6a976f31a995a6764f7
Justinas <justinas.smertinas@gmail.com>
2017-04-16 15:56:54 +0200

    New Algo.py that adds new methods
    The new method added returns a list of Y values from polynomial approximation.

--- main/algo.py
+++ main/algo.py
@@ -1,6 +1,7 @@
 # Function for the Method of least squares
 # by Justinas Smertinas
 # Edited by Ali Rassolie
+# Edited again by Justinas Smertinas
 
 #What to import from numpy.
 from numpy import array, transpose, dot, linalg, power
@@ -8,7 +9,7 @@ from numpy import array, transpose, dot, linalg, power
 #We want to find the best fitting polynomial of any order (that you choose)
 #between a set of point in the XY plane.
 #For this we employ a Method of Least Squares.
-#The following function returns a list of coefficients for a polynomial of
+#The following function returns a list of coefficients for a polynomial
 #of order chosen by the user.
 #Exanple of the return [a, b, c, d] where a is coefficient of the largest
 #degree term (in this case 3).
@@ -22,36 +23,48 @@ class Algo:
     def leastSquares(self, x=None, y=None, order=1):
         #We note the length of the input list so that we do not have to compute it again
         lengthOfList = len(x)
-        list_of_XY_coordinates = [[x[i],y[i]] for i in range(0, len(x))]
         #We set up 3 list that later will be turned into numpy
         # A transposed
-        # X values
-        # Y values
-        At = []
-        X = []
-        Y = []
 
-        #From the list of coordinates we split Xs and Ys into their respective lists.
-        #Do note that while X is just a list, Y is a list of lists
-        #as we later want it to be a colmn vector.
-        for i in range(lengthOfList):
-            X.append(list_of_XY_coordinates[i][0])
-            Y.append([list_of_XY_coordinates[i][1]])
+        At = []
 
         #We create A transposed list by appending list of X raised
         #to powers of descending order.
         for i in reversed(range(order+1)):
-            At.append(power(X, i))
+            At.append(power(x, i))
         #We turn At into a numpy array
         #And create its transversed version.
         At = array(At)
         A = At.transpose()
 
         #We solve a systen <At*A>=<At*Y>
-        solutionArray = linalg.solve(dot(At, A), dot(At, Y))
+        solutionArray = linalg.solve(dot(At, A), dot(At, y))
         #However we obtain solution in form of an Array and since I dislike it's format
         #I turn it into a simple list with the folowing for loop.
         solution = []
         for i in range(len(solutionArray)):
-            solution.append(solutionArray[i][0])
+            solution.append(solutionArray[i])
         return solution
+
+
+    #Returns a list of Y approximations from known X and Y for wanted x.
+    #We need a list of known X and Y values to find polynomial approximations
+    #and we also need a list of X values, for which we want approximated values.
+    def leastSquaresApproximationResults(self, xKnown = None, xWanted = None, yKnown=None, order = 1):
+        #We calculate the polynomial coefficients
+        coefficients = self.leastSquares(xKnown, yKnown, order)
+        #We reverse the list for later convenience when calculating approximated Y values
+        coefficients.reverse()
+        #We create a list that we will alter return
+        yApproxList = []
+        #We iterate through the list of wanted X Values
+        for i in xWanted:
+            #We calculate approximation of Y values for wanted X and append it to the list
+            yApprox = 0
+            for j in range(len(coefficients)):
+                yApprox += coefficients[j]*(i**j)
+            yApproxList.append(yApprox)
+        #We return the list of approximated Y values
+        return yApproxList
+
+
